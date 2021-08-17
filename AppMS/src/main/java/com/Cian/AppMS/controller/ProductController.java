45fab07f54761c896ea00ed46a70d4857de4bc4e
Cian <cnlynch20@gmail.com>
2021-08-17 10:44:42 +1000

    fixed formatting on ProductController

--- AppMS/src/main/java/com/Cian/AppMS/controller/ProductController.java
+++ AppMS/src/main/java/com/Cian/AppMS/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
+
     @GetMapping("/")
     public String pageDesu(Model model) {
         model.addAttribute("products", productRepository.findAll());
